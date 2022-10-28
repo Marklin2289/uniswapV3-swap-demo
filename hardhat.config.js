b28b4d9b281e76a17fbc15477bdf3d6c694bcf00
marklin2289 <marklin2289@gmail.com>
2022-10-27 20:16:51 -0700

    added SimpleSwap.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -113,7 +113,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.7",
+                version: "0.7.6",
             },
             {
                 version: "0.4.24",
